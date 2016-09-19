f3602d08f592ff78053b41ff6ae00939146374b8
luke-eivind <luke.eivind@gmail.com>
2016-09-19 00:50:39 -0500

    added first version

---
+++ Harambe1/Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerBehavior : MonoBehaviour {
+
+    public float speed = 2;
+    public float jumpForce = 1;
+    public float rotateSpeed = 1000;
+
+    public Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        move();
+        jump();
+	}
+
+    void move()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * speed);
+    }
+
+    void jump()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce((Vector2.up + Vector2.right) * jumpForce);
+            transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+        }
+    }
+}
