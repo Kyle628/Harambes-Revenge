c3a08741dbd3a585e2313308842d279f646fcd68
kyle <kyjoconn@ucsc.edu>
2016-09-26 15:41:36 -0700

    added double jump and infinite scrolling

---
+++ Harambe1/Assets/Scripts/GroundShiz.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundShiz : MonoBehaviour {
+
+	public GameObject moreGround;
+
+	void Start() {
+		Spawn ();
+	}
+
+	public void Spawn()
+	{
+		GameObject Ground = GameObject.Find("Ground");
+		moreGround = (GameObject)Instantiate(Ground, transform.position + new Vector3(25, 0, 0), Quaternion.identity);
+	}
+	
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Player") {
+			GameObject Player = GameObject.Find("Player");
+			PlayerBehavior playerBehavior = Player.GetComponent<PlayerBehavior>();
+			playerBehavior.grounded = true;
+			playerBehavior.doubleJump = false;
+		}
+
+	}
+}
--- Harambe1/Assets/Scripts/PlayerBehavior.cs
+++ Harambe1/Assets/Scripts/PlayerBehavior.cs
@@ -7,6 +7,8 @@ public class PlayerBehavior : MonoBehaviour {
     public float speed = 2;
     public float jumpForce = 1;
     public float rotateSpeed = 1000;
+	public bool grounded = true;
+	public bool doubleJump = false;
 
     public Rigidbody2D rb;
 
@@ -28,10 +30,17 @@ public class PlayerBehavior : MonoBehaviour {
 
     void jump()
     {
-        if(Input.GetButtonDown("Jump"))
+		if(Input.GetButtonDown("Jump") && (grounded))
         {
             rb.AddForce((Vector2.up + Vector2.right) * jumpForce);
             transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
-        }
+			grounded = false;
+		} else if (Input.GetButtonDown("Jump") && (!doubleJump))
+		{
+			rb.velocity = new Vector3(rb.velocity.x, 0, 0);
+			rb.AddForce((Vector2.up + Vector2.right) * jumpForce);
+			transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+			doubleJump = true;
+		}
     }
 }
