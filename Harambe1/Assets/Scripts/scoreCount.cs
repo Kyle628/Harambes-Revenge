011ebb887d1d303612f0b173ea1e2afc0fb79cd3
kyle <kyjoconn@ucsc.edu>
2016-10-19 11:42:21 -0700

    got a score displayed, doesn't increment yet though

---
+++ Harambe1/Assets/Scripts/scoreCount.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class scoreCount : MonoBehaviour {
+	private Text score;
+	private float nextTime = 0f;
+	// Use this for initialization
+	void Start () {
+		score.GetComponent<UnityEngine.UI.Text> ().text;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time >= nextTime) {
+
+			//do something here every interval seconds
+			score = "hi";
+			nextTime += 1; 
+
+		}
+	
+	}
+}
